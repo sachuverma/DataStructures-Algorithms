4181b1005811a5a9f0e6b62688a9bef61205d11a
SAM Bhardwaj <bsamrat47@gmail.com>
2020-10-09 10:48:34 +0530

    Samrat

---
+++ Leetcode-Daily-Challenge/October-2020/Day-8.java
@@ -0,0 +1,20 @@
+class Day-8 {
+    class Solution {
+        public int search(int[] nums, int target) {
+            int low=0;
+            int high=nums.length-1;
+            
+            while(low<=high){
+                int m=(low+high)/2;
+                if(target<nums[m]){
+                    high=m-1;
+                } else if(nums[m]<target){
+                    low=m+1;
+                } else {
+                    return m;
+                }
+            }
+            return -1;
+        }
+    }
+}
