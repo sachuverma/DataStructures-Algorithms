9cbfd36b9342a880eed62b5ee6d5f08647b1a3a3
sachinverma53121 <sachinverma53121@gmail.com>
2020-10-16 21:26:02 +0530

    min jumps

--- DSA-Crack-Sheet/10. Minimum Number Of Jumps.cpp
+++ DSA-Crack-Sheet/10. Minimum Number Of Jumps.cpp
@@ -1,3 +1,45 @@
+// O(n^n)
+#include <bits/stdc++.h>
+using namespace std;
+
+int minJumps(vector<int> arr, int n)
+{
+  if (n == 1)
+    return 0;
+
+  int res = INT_MAX;
+  for (int i = n - 2; i >= 0; i--)
+  {
+    if (i + arr[i] >= n - 1)
+    {
+      int sub_res = minJumps(arr, i + 1);
+      if (sub_res != INT_MAX)
+        res = min(res, sub_res + 1);
+    }
+  }
+
+  return res;
+}
+
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (auto &i : arr)
+      cin >> i;
+
+    cout << minJumps(arr, n) << endl;
+  }
+  return 0;
+}
+
+// ===================================
+// O(n2)
 #include <bits/stdc++.h>
 using namespace std;
 
