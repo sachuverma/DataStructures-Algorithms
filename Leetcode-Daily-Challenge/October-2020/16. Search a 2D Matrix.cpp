0b89134f74e7e06b35f46d7426f89d9ddda30141
sachinverma53121 <sachinverma53121@gmail.com>
2020-10-16 18:46:25 +0530

    day 16

---
+++ Leetcode-Daily-Challenge/October-2020/16. Search a 2D Matrix.cpp
@@ -0,0 +1,54 @@
+/*
+Search a 2D Matrix
+==================
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+
+
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 3
+Output: true
+
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 13
+Output: false
+
+Example 3:
+Input: matrix = [], target = 0
+Output: false
+
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+0 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+
+*/
+
+class Solution
+{
+public:
+  bool searchMatrix(vector<vector<int>> &matrix, int target)
+  {
+    if (matrix.size() == 0)
+      return false;
+    int m = matrix.size(), n = matrix[0].size();
+    int r = 0, c = n - 1;
+
+    while (r < m && c >= 0)
+    {
+      if (matrix[r][c] == target)
+        return true;
+      else if (matrix[r][c] < target)
+        r++;
+      else
+        c--;
+    }
+
+    return false;
+  }
+};
--- Leetcode-Daily-Challenge/October-2020/README.md
+++ Leetcode-Daily-Challenge/October-2020/README.md
@@ -16,6 +16,7 @@
 > [Sort List](https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/560/week-2-october-8th-october-14th/3493/) - [Cpp Solution](./13.%20Sort%20List.cpp)     
 > [House Robber II](https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/560/week-2-october-8th-october-14th/3494/) - [Cpp Solution](./14.%20House%20Robber%20II.cpp)     
 > [Rotate Array](https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/561/week-3-october-15th-october-21st/3496/) - [Cpp Solution](./15.%20Rotate%20Array.cpp)     
+> [Search a 2D Matrix](https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/561/week-3-october-15th-october-21st/3497/) - [Cpp Solution](./16.%20Search%20a%202D%20Matrix.cpp)     
 
 
 
