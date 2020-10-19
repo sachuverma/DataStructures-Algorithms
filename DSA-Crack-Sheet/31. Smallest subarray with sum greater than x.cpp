20dd9bbeaccd2d68b13f33cb86628be73654e414
sachinverma53121 <sachinverma53121@gmail.com>
2020-10-19 19:43:50 +0530

    sum greater than x

--- DSA-Crack-Sheet/31. Smallest subarray with sum greater than x.cpp
+++ DSA-Crack-Sheet/31. Smallest subarray with sum greater than x.cpp
@@ -1,3 +1,4 @@
+// O(n2)
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -22,6 +23,42 @@ int main()
         sum += arr[j];
         if (sum > x)
           ans = min(ans, j - i + 1);
+      }
+    }
+    cout << ans << endl;
+  }
+  return 0;
+}
+
+/* =================================== */
+// O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n, x;
+    cin >> n >> x;
+    vector<int> arr(n);
+    for (auto &i : arr)
+      cin >> i;
+
+    int curr_sum = 0, ans = n + 1;
+    int start = 0, end = 0;
+    while (end < n)
+    {
+      while (curr_sum <= x && end < n)
+        curr_sum += arr[end++];
+      while (curr_sum > x && start < n)
+      {
+        if (end - start < ans)
+          ans = end - start;
+        curr_sum -= arr[start++];
       }
     }
     cout << ans << endl;
