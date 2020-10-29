b14abc30bebbf83eea791b9126de98c23b4207cc
sachinverma53121 <sachinverma53121@gmail.com>
2020-10-29 20:52:41 +0530

    day 29

--- Leetcode-Daily-Challenge/October-2020/29. Maximize distance to Closest Person.cpp
+++ Leetcode-Daily-Challenge/October-2020/29. Maximize distance to Closest Person.cpp
@@ -1,12 +1,11 @@
 /*
-  MAXIMISE DISTANCE TO CLOSEST PERSON
-  ========================================
-  You are given an array representing a row of seats where seats[i] = 1 represents a person sitting in the ith seat, and seats[i] = 0 represents that the ith seat is empty (0-indexed).
+MAXIMISE DISTANCE TO CLOSEST PERSON
+===================================
+
+You are given an array representing a row of seats where seats[i] = 1 represents a person sitting in the ith seat, and seats[i] = 0 represents that the ith seat is empty (0-indexed).
 
 There is at least one empty seat, and at least one person sitting.
-
 Alex wants to sit in the seat such that the distance between him and the closest person to him is maximized.
-
 Return that maximum distance to the closest person.
 
 Example-1:
@@ -28,36 +27,38 @@ Example-3
 Input: seats = [0,1]
 Output: 1
 
-========X=========X========X========X========
-SOLUTION:
-
 INTUTION:
 Count number of zeros between two adjacent ones and store maximum of half of count in an answer variable.
 Take care of edge cases like number of zeros in starting or in end.
 
 TIME COMPLEXITY:O(n)
 SPACE COMPLEXITY:O(1)
-
-C++:
 */
 
-class Solution {
+class Solution
+{
 public:
-    int maxDistToClosest(vector<int>& seats) {
-        int i=0;
-        int ans=0;
-        int count=0;
-        while(seats[i]==0) i++;
-        ans=i;
-        int n=seats.size();
-        for(;i<n;i++){
-            if(seats[i]==1){
-                ans=max(ans,(count+1)/2);
-                count=0;
-            }else
-                count++;
-        }
-        ans=max(ans,count);
-        return ans;
+  int maxDistToClosest(vector<int> &seats)
+  {
+    int i = 0;
+    int ans = 0;
+    int count = 0;
+    while (seats[i] == 0)
+      i++;
+
+    ans = i;
+    int n = seats.size();
+    for (; i < n; i++)
+    {
+      if (seats[i] == 1)
+      {
+        ans = max(ans, (count + 1) / 2);
+        count = 0;
+      }
+      else
+        count++;
     }
+    ans = max(ans, count);
+    return ans;
+  }
 };
