5d3b57f8fabd955e594967dd5a5f98fb702b109d
Sachin Verma <sachinverma53121@gmail.com>
2020-10-31 12:28:09 -0700

    day 30

--- Leetcode-Daily-Challenge/October-2020/30. Number of Longest Increasing Subsequence.cpp
+++ Leetcode-Daily-Challenge/October-2020/30. Number of Longest Increasing Subsequence.cpp
@@ -20,33 +20,42 @@ Constraints:
 -106 <= nums[i] <= 106
 */
 
-class Solution {
+class Solution
+{
 public:
-  int findNumberOfLIS(vector<int>& nums) {
-    int n=nums.size();
-    if(n<2) return n;
-    
-    vector<pair<int,int>>dp(n,{1,1});
-    int maxlen=1;
-    int ans=1;
-    
-    for(int i=1;i<n;i++){
-      for(int j=i-1;j>=0;j--){
-        if(nums[j]>=nums[i]) continue;
-        if(dp[j].first+1>dp[i].first) {
-          dp[i].first=dp[j].first+1;
-          dp[i].second=dp[j].second;
+  int findNumberOfLIS(vector<int> &nums)
+  {
+    int n = nums.size();
+    if (n < 2)
+      return n;
+
+    vector<pair<int, int>> dp(n, {1, 1});
+    int maxlen = 1;
+    int ans = 1;
+
+    for (int i = 1; i < n; i++)
+    {
+      for (int j = i - 1; j >= 0; j--)
+      {
+        if (nums[j] >= nums[i])
+          continue;
+        if (dp[j].first + 1 > dp[i].first)
+        {
+          dp[i].first = dp[j].first + 1;
+          dp[i].second = dp[j].second;
         }
-        else if(dp[j].first+1==dp[i].first) dp[i].second+=dp[j].second;
+        else if (dp[j].first + 1 == dp[i].first)
+          dp[i].second += dp[j].second;
       }
-      
-      if(dp[i].first>maxlen){
-        maxlen=dp[i].first;
-        ans=dp[i].second;
+
+      if (dp[i].first > maxlen)
+      {
+        maxlen = dp[i].first;
+        ans = dp[i].second;
       }
-      else if(dp[i].first==maxlen) ans+=dp[i].second;
+      else if (dp[i].first == maxlen)
+        ans += dp[i].second;
     }
     return ans;
   }
 };
-
