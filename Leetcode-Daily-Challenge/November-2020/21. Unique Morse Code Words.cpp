af8864061a53d7d2d88db16d8ac576a6eb3b8a1e
Sachin Verma <sachinverma53121@gmail.com>
2020-11-22 17:04:09 +0530

    day 21

---
+++ Leetcode-Daily-Challenge/November-2020/21. Unique Morse Code Words.cpp
@@ -0,0 +1,50 @@
+/*
+Unique Morse Code Words
+=======================
+
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-..--...", (which is the concatenation "-.-." + ".-" + "-..."). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation: 
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+
+Note:
+The length of words will be at most 100.
+Each words[i] will have length in range [1, 12].
+words[i] will only consist of lowercase letters.
+*/
+
+class Solution
+{
+public:
+  int uniqueMorseRepresentations(vector<string> &words)
+  {
+    set<string> uniqueCodes;
+    vector<string> morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+    for (auto &word : words)
+    {
+      string subans;
+      for (auto &c : word)
+        subans += morse[c - 'a'];
+      uniqueCodes.insert(subans);
+    }
+
+    return uniqueCodes.size();
+  }
+};
