81109771df01e7fc42cfce9bacbed6e245fe0ab4
Sachin Verma <sachinverma53121@gmail.com>
2020-12-02 21:35:48 +0530

    day 20

---
+++ Leetcode Daily Challenge/November-2020/20. Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,32 @@
+/*
+Search in Rotated Sorted Array II
+=================================
+
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+(i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+You are given a target value to search. If found in the array return true, otherwise return false.
+
+Example 1:
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+
+Example 2:
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+
+Follow up:
+This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+Would this affect the run-time complexity? How and why?
+*/
+
+class Solution
+{
+public:
+  bool search(vector<int> &nums, int target)
+  {
+    for (auto &i : nums)
+      if (i == target)
+        return true;
+    return false;
+  }
+};
