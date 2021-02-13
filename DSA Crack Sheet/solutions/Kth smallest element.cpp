45ba605c8f96a5a702247d42798038f87fec24a6
Sachin Verma <sachinverma53121@gmail.com>
2021-02-13 20:36:48 +0530

    Kth smallest element

--- DSA Crack Sheet/solutions/Kth smallest element.cpp
+++ DSA Crack Sheet/solutions/Kth smallest element.cpp
@@ -40,6 +40,7 @@ Constraints:
 // l : starting index of the array i.e 0
 // r : ending index of the array i.e size-1
 // k : find kth smallest element and return using this function
+
 int kthSmallest(int arr[], int l, int r, int k)
 {
   sort(arr + l, arr + r + 1);
