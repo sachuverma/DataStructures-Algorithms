359c1703fa23a5df9646eeb7fb58ba7c2b14e14a
Sachin Verma <sachinverma53121@gmail.com>
2021-02-13 18:06:04 +0530

    dsa carck sheet

--- DSA Crack Sheet/solutions/91. Value equal to index value.cpp
+++
@@ -1,9 +0,0 @@
-
-vector<int> valueEqualToIndex(int arr[], int n)
-{
-  vector<int> ans;
-  for (int i = 0; i < n; ++i)
-    if (arr[i] == i + 1)
-      ans.push_back(i + 1);
-  return ans;
-}
--- DSA Crack Sheet/solutions/93. Count Squares.cpp
+++
@@ -1,6 +0,0 @@
-
-int countSquares(int N)
-{
-  int n = ceil(sqrt(N));
-  return n - 1;
-}
