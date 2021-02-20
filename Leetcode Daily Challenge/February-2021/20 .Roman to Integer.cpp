631ae7f7c0bf048be4f3e1c206314d96cc1d1f28
Sachin Verma <sachinverma53121@gmail.com>
2021-02-20 17:53:29 +0530

    day 20

---
+++ Leetcode Daily Challenge/February-2021/20 .Roman to Integer.cpp
@@ -0,0 +1,85 @@
+/*
+Roman to Integer
+================
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+Example 1:
+Input: s = "III"
+Output: 3
+
+Example 2:
+Input: s = "IV"
+Output: 4
+
+Example 3:
+Input: s = "IX"
+Output: 9
+
+Example 4:
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 5:
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+Constraints:
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+Hint #1  
+Problem is simpler to solve by working the string from back to front and using a map.
+*/
+
+class Solution
+{
+public:
+  int romanToInt(string s)
+  {
+    unordered_map<char, int> value;
+    value['I'] = 1;
+    value['V'] = 5;
+    value['X'] = 10;
+    value['L'] = 50;
+    value['C'] = 100;
+    value['D'] = 500;
+    value['M'] = 1000;
+
+    int ans = 0;
+    int prev_int = value[s[s.size() - 1]];
+    ans += prev_int;
+
+    for (int i = s.size() - 2; i >= 0; --i)
+    {
+      int curr_int = value[s[i]];
+      if (curr_int >= prev_int)
+        ans += curr_int;
+      else
+        ans -= curr_int;
+      prev_int = curr_int;
+    }
+
+    return ans;
+  }
+};
