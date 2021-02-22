20606162f7db418b3df15b7f46ee9b665c481474
Sachin Verma <sachinverma53121@gmail.com>
2021-02-22 13:08:38 +0530

    day 21

---
+++ Leetcode Daily Challenge/February-2021/Broken Calculator.cpp
@@ -0,0 +1,56 @@
+/*
+Broken Calculator
+=================
+
+On a broken calculator that has a number showing on its display, we can perform two operations:
+
+Double: Multiply the number on the display by 2, or;
+Decrement: Subtract 1 from the number on the display.
+Initially, the calculator is displaying the number X.
+
+Return the minimum number of operations needed to display the number Y.
+
+Example 1:
+Input: X = 2, Y = 3
+Output: 2
+Explanation: Use double operation and then decrement operation {2 -> 4 -> 3}.
+
+Example 2:
+Input: X = 5, Y = 8
+Output: 2
+Explanation: Use decrement and then double {5 -> 4 -> 8}.
+
+Example 3:
+Input: X = 3, Y = 10
+Output: 3
+Explanation:  Use double, decrement and double {3 -> 6 -> 5 -> 10}.
+
+Example 4:
+Input: X = 1024, Y = 1
+Output: 1023
+Explanation: Use decrement operations 1023 times.
+
+Note:
+1 <= X <= 10^9
+1 <= Y <= 10^9
+*/
+
+class Solution
+{
+public:
+  int brokenCalc(int x, int y)
+  {
+    int count = 0;
+    while (y != x)
+    {
+      if (x >= y)
+        return ((x - y) + count);
+      if (y % 2 == 0)
+        y = y / 2;
+      else
+        y++;
+      count++;
+    }
+    return count;
+  }
+};
