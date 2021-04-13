3c7a528c1df0df6b677b1de0af2d069cc390ce19
Sachin Verma <sachinverma53121@gmail.com>
2021-04-13 16:08:45 +0530

    day 13

---
+++ Leetcode Daily Challenge/April-2021/13. Flatten Nested List Iterator.cpp
@@ -0,0 +1,85 @@
+/*
+Flatten Nested List Iterator
+============================
+
+You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists. Implement an iterator to flatten it.
+
+Implement the NestedIterator class:
+NestedIterator(List<NestedInteger> nestedList) Initializes the iterator with the nested list nestedList.
+int next() Returns the next integer in the nested list.
+boolean hasNext() Returns true if there are still some integers in the nested list and false otherwise.
+
+Example 1:
+Input: nestedList = [[1,1],2,[1,1]]
+Output: [1,1,2,1,1]
+Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+
+Example 2:
+Input: nestedList = [1,[4,[6]]]
+Output: [1,4,6]
+Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
+
+Constraints:
+1 <= nestedList.length <= 500
+The values of the integers in the nested list is in the range [-106, 106].
+*/
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedIterator
+{
+public:
+  vector<int> ans;
+  int curr;
+
+  void flatten(vector<NestedInteger> &nestedList)
+  {
+    for (NestedInteger &i : nestedList)
+    {
+      if (i.isInteger())
+        ans.push_back(i.getInteger());
+      else
+        flatten(i.getList());
+    }
+  }
+
+  NestedIterator(vector<NestedInteger> &nestedList)
+  {
+    curr = 0;
+    flatten(nestedList);
+  }
+
+  int next()
+  {
+    int res = ans[curr];
+    curr++;
+    return res;
+  }
+
+  bool hasNext()
+  {
+    return curr < ans.size();
+  }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
--- Leetcode Daily Challenge/April-2021/README.md
+++ Leetcode Daily Challenge/April-2021/README.md
@@ -14,3 +14,4 @@
 | 10.  | [Longest Increasing Path in a Matrix](https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/594/week-2-april-8th-april-14th/3703/)   |  [cpp](./10.%20Longest%20Increasing%20Path%20in%20a%20Matrix.cpp)  |
 | 11.  | [Deepest Leaves Sum](https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/594/week-2-april-8th-april-14th/3704/)   |  [cpp](./11.%20Deepest%20Leaves%20Sum.cpp)  |
 | 12.  | [Beautiful Arrangement II](https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/594/week-2-april-8th-april-14th/3705/)   |  [cpp](./12.%20Beautiful%20Arrangement%20II.cpp)  |
+| 13.  | [Flatten Nested List Iterator](https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/594/week-2-april-8th-april-14th/3706/)   |  [cpp](./13.%20Flatten%20Nested%20List%20Iterator.cpp)  |
