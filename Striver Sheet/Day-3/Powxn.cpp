8d38d2f7f46323908d48609525c5b4adba609065
Sachin Verma <sachinverma53121@gmail.com>
2021-04-23 21:58:22 +0530

    pow of xn

--- Striver Sheet/Day-3/Powxn.cpp
+++ Striver Sheet/Day-3/Powxn.cpp
@@ -22,3 +22,31 @@ Constraints:
 -231 <= n <= 231-1
 -104 <= xn <= 104
 */
+
+class Solution
+{
+public:
+  double myPow(double x, int n)
+  {
+    long long N = abs(n);
+    double ans = 1.0;
+    while (N > 0)
+    {
+      if (N % 2 == 1)
+      {
+        ans = ans * x;
+        N--;
+      }
+      else
+      {
+        x = x * x;
+        N = N / 2;
+      }
+    }
+
+    if (n < 0)
+      return 1.0 / ans;
+    else
+      return ans;
+  }
+};
