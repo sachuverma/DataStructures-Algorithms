f70e1df8f4f261c598835403e57e8c1afd598406
Sachin Verma <sachinverma53121@gmail.com>
2021-04-27 20:53:46 +0530

    deep copy LL

--- Striver Sheet/Day-7/Copy List with Random Pointer.cpp
+++ Striver Sheet/Day-7/Copy List with Random Pointer.cpp
@@ -39,3 +39,86 @@ Constraints:
 Node.random is null or is pointing to some node in the linked list.
 */
 
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+  Node *copyRandomList(Node *head)
+  {
+
+    Node *newCopy = new Node(-1);
+    auto temp = newCopy;
+    auto temp_o = head;
+
+    // copy bngyi ek next use krke bss
+    while (temp_o)
+    {
+      Node *newNode = new Node(temp_o->val);
+      temp->next = newNode;
+      temp = temp->next;
+      temp_o = temp_o->next;
+    }
+
+    newCopy = newCopy->next;
+
+    // 7 -> 13 -> 11 -> 10 -> 1
+    temp = newCopy;
+    temp_o = head;
+
+    while (temp)
+    {
+      auto next = temp->next;
+      auto next_o = temp_o->next;
+
+      temp_o->next = temp;
+      temp->next = next_o;
+
+      temp_o = next_o;
+      temp = next;
+    }
+
+    temp = newCopy;
+    temp_o = head;
+
+    while (temp_o)
+    {
+      auto new_rand = temp_o->random;
+      if (new_rand)
+        temp->random = new_rand->next;
+      if (temp_o->next)
+        temp_o = temp_o->next->next;
+      if (temp->next)
+        temp = temp->next->next;
+    }
+
+    temp = newCopy;
+    temp_o = head;
+
+    while (temp)
+    {
+      if (temp_o->next)
+        temp_o->next = temp_o->next->next;
+      if (temp->next)
+        temp->next = temp->next->next;
+      temp_o = temp_o->next;
+      temp = temp->next;
+    }
+
+    return newCopy;
+  }
+};
