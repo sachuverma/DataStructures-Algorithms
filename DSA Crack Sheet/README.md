00008cabccf7a1df98c5809c3b22aecf936336bd
Sachin Verma <sachinverma53121@gmail.com>
2021-06-17 18:41:31 +0530

    day 16

--- DSA Crack Sheet/README.md
+++ DSA Crack Sheet/README.md
@@ -173,6 +173,7 @@
 - [Rotten Oranges](https://practice.geeksforgeeks.org/problems/rotten-oranges2536/1 "view question") - [Cpp Solution](./solutions/Rotten%20Oranges.cpp)
 - [Distance of nearest cell having 1](https://practice.geeksforgeeks.org/problems/distance-of-nearest-cell-having-1-1587115620/1# "view question") - [Cpp Solution](./solutions/Distance%20of%20nearest%20cell%20having%201.cpp)
 - [First negative integer in every window of size k](https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1# "view question") - [Cpp Solution](./solutions/First%20negative%20integer%20in%20every%20window%20of%20size%20k.cpp)
+- [Check if all levels of two trees are anagrams or not](https://www.geeksforgeeks.org/check-if-all-levels-of-two-trees-are-anagrams-or-not/ "view topic")
 - []( "view question") - [Cpp Solution](./solutions/.cpp)
 
 ### Heap
---
+++ Leetcode Daily Challenge/June-2021/17. Number of Subarrays with Bounded Maximum.cpp
@@ -0,0 +1,49 @@
+/*
+Number of Subarrays with Bounded Maximum
+=========================================
+
+We are given an array nums of positive integers, and two positive integers left and right (left <= right).
+
+Return the number of (contiguous, non-empty) subarrays such that the value of the maximum array element in that subarray is at least left and at most right.
+
+Example:
+Input: 
+nums = [2, 1, 4, 3]
+left = 2
+right = 3
+Output: 3
+Explanation: There are three subarrays that meet the requirements: [2], [2, 1], [3].
+Note:
+
+left, right, and nums[i] will be an integer in the range [0, 109].
+The length of nums will be in the range of [1, 50000].
+*/
+
+class Solution
+{
+public:
+  int numSubarrayBoundedMax(vector<int> &nums, int left, int right)
+  {
+    int ans = 0;
+    int i = 0, j = 0, count = 0;
+
+    while (j < nums.size())
+    {
+      if (nums[j] >= left && nums[j] <= right)
+      {
+        count = (j - i + 1);
+        ans += count;
+      }
+      else if (nums[j] < left)
+        ans += count;
+      else
+      {
+        i = j + 1;
+        count = 0;
+      }
+      j++;
+    }
+
+    return ans;
+  }
+};
