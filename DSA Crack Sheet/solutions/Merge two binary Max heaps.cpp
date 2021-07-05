f2a8a12ae54507147779962021c23ba6c092a1da
Sachin Verma <sachinverma53121@gmail.com>
2021-07-05 20:44:11 +0530

    day 5

---
+++ DSA Crack Sheet/solutions/Merge two binary Max heaps.cpp
@@ -0,0 +1,25 @@
+/*
+Merge two binary Max heaps
+==========================
+
+Given two binary max heaps as arrays, merge the given heaps to form a new max heap.
+
+Example 1:
+Input  : 
+n = 4 m = 3
+a[] = {10, 5, 6, 2}, 
+b[] = {12, 7, 9}
+Output : 
+{12, 10, 9, 2, 5, 7, 6}
+Explanation :
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function mergeHeaps() which takes the array a[], b[], its size n and m, as inputs and return the merged max heap.
+
+Expected Time Complexity: O(n.Logn)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= n, m <= 105
+1 <= a[i], b[i] <= 2*105
+*/
