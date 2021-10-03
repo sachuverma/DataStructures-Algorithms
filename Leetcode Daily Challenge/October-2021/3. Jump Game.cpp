67067ce4332ae7000e17f50c97034631eeb730fb
Sachin Verma <43083013+sachuverma@users.noreply.github.com>
2021-10-03 21:10:54 +0530

    Update and rename jumpGame.cpp to 3. Jump Game.cpp

--- Leetcode Daily Challenge/October-2021/3. Jump Game.cpp
+++ Leetcode Daily Challenge/October-2021/3. Jump Game.cpp
@@ -22,11 +22,10 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        
         int vis = n-1;
         
-        for(int i=n-2;i>=0;i--) {
-            if(i+nums[i]>=vis)
+        for(int i = n-2; i >= 0; i--) {
+            if(i+nums[i] >= vis)
                 vis = i;
         }
         
