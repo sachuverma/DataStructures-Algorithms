5f1c6a74133956c1c6defae674341d1c79dc2fa1
Sachin Verma <43083013+sachuverma@users.noreply.github.com>
2021-10-07 10:01:58 +0530

    Update and rename 6DuplicatesInArray.cpp to 6. Duplicates In Array.cpp

--- Leetcode Daily Challenge/October-2021/6. Duplicates In Array.cpp
+++ Leetcode Daily Challenge/October-2021/6. Duplicates In Array.cpp
@@ -1,20 +1,17 @@
 
-/*Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
-
+/*
+Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, 
+return an array of all the integers that appears twice.
 You must write an algorithm that runs in O(n) time and uses only constant extra space.
 
- 
-
 Example 1:
-
 Input: nums = [4,3,2,7,8,2,3,1]
 Output: [2,3]
+
 Example 2:
-
 Input: nums = [1,1,2]
-Output: [1] */
-
-
+Output: [1] 
+*/
 
 class Solution {
 public:
