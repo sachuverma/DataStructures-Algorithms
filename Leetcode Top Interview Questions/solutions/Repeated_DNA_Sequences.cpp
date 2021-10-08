1be1d6b43397560ba8811d7658c5f703405648c9
Sachin Verma <43083013+sachuverma@users.noreply.github.com>
2021-10-09 00:26:43 +0530

    Update Repeated_DNA_Sequences.cpp

--- Leetcode Top Interview Questions/solutions/Repeated_DNA_Sequences.cpp
+++ Leetcode Top Interview Questions/solutions/Repeated_DNA_Sequences.cpp
@@ -3,15 +3,15 @@ public:
     vector<string> findRepeatedDnaSequences(string s) {
         unordered_map <string, int> freq;
         vector<string> out;
+        
         if(s.size()<11){
             return out;
         }
+        
         for(int i=0; i<s.size()-9; i++){
             string temp = s.substr(i, 1+9);
             freq[temp]++;
         }
-        
-        
         
         for(auto x : freq){
             if(x.second>1){
