5ceda73feaf85f39283e037f724282a4f620be9f
Sachin Verma <43083013+sachuverma@users.noreply.github.com>
2021-10-08 15:49:18 +0530

    Update 7. Word Search.cpp

--- Leetcode Daily Challenge/October-2021/7. Word Search.cpp
+++ Leetcode Daily Challenge/October-2021/7. Word Search.cpp
@@ -19,27 +19,27 @@ Example 2:
 Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
 Output: false
 */
+
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-         for (unsigned int i = 0; i < board.size(); i++) 
+      for (unsigned int i = 0; i < board.size(); i++) 
         for (unsigned int j = 0; j < board[0].size(); j++) 
             if (dfs(board, i, j, word))
                 return true;
-    return false;
-}
+      return false;
+    }
 
-bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
-    if (!word.size())
-        return true;
-    if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
-        return false;
-    char c = board[i][j];
-    board[i][j] = '*';
-    string s = word.substr(1);
-    bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
-    board[i][j] = c;
-    return ret;
-}
-    
+    bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
+        if (!word.size())
+            return true;
+        if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
+            return false;
+        char c = board[i][j];
+        board[i][j] = '*';
+        string s = word.substr(1);
+        bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
+        board[i][j] = c;
+        return ret;
+    }
 };
