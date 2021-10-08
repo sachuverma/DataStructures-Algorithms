c4771862d99385b443dc155a30a76979d2418c50
Akshad Nayakwadi <73118552+Akshad7829@users.noreply.github.com>
2021-10-08 13:57:59 +0530

    Update 7. Word Search.cpp
    made changes in code format

--- Leetcode Daily Challenge/October-2021/7. Word Search.cpp
+++ Leetcode Daily Challenge/October-2021/7. Word Search.cpp
@@ -1,3 +1,24 @@
+/*
+                                                        #Hacktoberfest2021
+                                                        
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ 
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+
+Example 2:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+*/
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
