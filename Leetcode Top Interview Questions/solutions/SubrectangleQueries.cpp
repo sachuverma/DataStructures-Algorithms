5d460f95bf2dc22858597c11f5c90b52c8d250d3
Shivam Rastogi <62622351+OmShivamR24@users.noreply.github.com>
2021-10-14 21:46:43 +0530

    Indented the code wherever needed to make code more readable .

--- Leetcode Top Interview Questions/solutions/SubrectangleQueries.cpp
+++ Leetcode Top Interview Questions/solutions/SubrectangleQueries.cpp
@@ -27,20 +27,20 @@ private:
 // Track Updates rather than updating the original data structure .. approach.
 
 class SubrectangleQueries {
-public:
-vector<vector<int>> rect, subs;
-SubrectangleQueries(vector<vector<int>>& rectangle) {
-    swap(rect, rectangle);
-}
-void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-    subs.push_back({row1, col1, row2, col2, newValue});
-}
-int getValue(int row, int col) {
-    for (int i = subs.size() - 1; i >= 0; --i)
-        if (subs[i][0] <= row && subs[i][1] <= col && row <= subs[i][2] && col <= subs[i][3])
-            return subs[i][4];
-    return rect[row][col];
-}
+    public:
+    vector<vector<int>> rect, subs;
+    SubrectangleQueries(vector<vector<int>>& rectangle) {
+        swap(rect, rectangle);
+    }
+    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        subs.push_back({row1, col1, row2, col2, newValue});
+    }
+    int getValue(int row, int col) {
+        for (int i = subs.size() - 1; i >= 0; --i)
+            if (subs[i][0] <= row && subs[i][1] <= col && row <= subs[i][2] && col <= subs[i][3])
+                return subs[i][4];
+        return rect[row][col];
+    }
 };
 
 /**
