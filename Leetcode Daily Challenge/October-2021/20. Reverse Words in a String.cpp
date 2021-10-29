9fea4895ee1217cfb73673e10c1aa412a545050d
kannu priya <imkannupriya5@gmail.com>
2021-10-29 16:09:20 +0530

    20th Oct 2021

---
+++ Leetcode Daily Challenge/October-2021/20. Reverse Words in a String.cpp
@@ -0,0 +1,61 @@
+/*
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+Example 4:
+
+Input: s = "  Bob    Loves  Alice   "
+Output: "Alice Loves Bob"
+Example 5:
+
+Input: s = "Alice does not even like bob"
+Output: "bob like even not does Alice"
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+ */
+
+string reverseWords(string s) {
+        if(s.size() == 0) return s;
+        stack<string> stack;
+        string result;
+        for(int i=0; i<s.size(); i++) {
+            string word;
+            if(s[i]==' ') continue; //skip spaces
+            while(i<s.size() && s[i]!=' ' ) { //store continuous letters into word
+                word += s[i]; i++;
+            }
+            stack.push(word); //push word to the stack
+        }
+        while(!stack.empty()) {
+            result += stack.top(); stack.pop();
+            if(!stack.empty()) result += " ";
+        }
+        return result;
+    }
+};
